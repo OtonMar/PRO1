e20be11a4fbc274ee49309d96becc356f406051a
“airaction” <otonmar201@gmail.com>
2023-02-13 09:37:43 +0100

    vaja

---
+++ VajaIzpis/VajaIzpis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaIzpis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 20;
+            double b = -13.6667;
+            double c = 17;
+            double d = 12.88;
+            double e = 2222.879;
+
+            Console.WriteLine("{0,20}", a);
+            Console.WriteLine("{0,20}", b);
+            Console.WriteLine("{0,20}", c);
+            Console.WriteLine("{0,20}", d);
+            Console.WriteLine("{0,20}", e);
+
+            double r = 12345.6789;
+            Console.WriteLine("r={0,10:f2}",r);
+            //f2-za 2 decimalni mesti, x- hexadecimalno, d-decimalno, e- št*10 na N, n- itpiše s . in ,, p-izpiše kot procent     
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VajaIzpis/VajaIzpis/Vaja10.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaja10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int abc=0;
+            String x;
+            Console.WriteLine("Vnesite temperaturo vode (od 5-100)");
+            x = Console.ReadLine();
+            int a = Convert.ToInt32(x);
+            if (a <= 10)
+            {
+                Console.WriteLine("zelo mrzla");
+                abc = 1;
+            }
+            else if (10<a & a<=20)
+            {
+                Console.WriteLine("hladna");
+                abc = 2;
+            }
+            else if (20 < a & a <= 30)
+            {
+                Console.WriteLine("topla");
+                abc = 3;
+            }
+            else if (30 < a & a <= 50)
+            {
+                Console.WriteLine("vroča");
+                abc = 4;
+            }
+            else if (a > 50)
+            {
+                Console.WriteLine("zelo vroča");
+                abc = 5;
+            }
+
+
+            switch (abc)
+            { 
+                case 1:
+                    Console.WriteLine("zelo mrzla");
+                    break;
+
+                case 2:
+                    Console.WriteLine("hladna");
+                    break;
+
+                case 3:
+                    Console.WriteLine("topla");
+                    break;
+
+                case 4:
+                    Console.WriteLine("vroča");
+                    break;
+
+                case 5:
+                    Console.WriteLine("zelo vroča");
+                    break;
+            }
+            Console.ReadLine();
+
+            
+        }
+    }
+}
