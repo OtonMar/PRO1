5448c3512410ff68cfb792fc1454a0c92eb6374b
“airaction” <otonmar201@gmail.com>
2023-02-13 10:20:47 +0100

    vaja

--- Prvi/Prvi/Program.cs
+++ Prvi/Prvi/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,31 @@ namespace Prvi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Rad imam c#");
+            int vsota = 0;
+            int i = 0;
+            int x;
+            
+            while (i < 10)
+            {
+                i++;
+                Console.WriteLine("vpišite " +i+". število");
+                x =int.Parse( Console.ReadLine());
+                vsota = vsota + x;
+            }
+            Console.WriteLine("vsota števil je "+vsota);
+            Console.ReadLine();
+            i = 0;
+            vsota = 0;
+            do
+            {
+                i++;
+                Console.WriteLine("vpišite " + i + ". število");
+                x = int.Parse(Console.ReadLine());
+                vsota = vsota + x;
+            } while (i < 10);
+            Console.WriteLine("vsota števil je " + vsota);
+            Console.ReadLine();
         }
+        
     }
 }
---
+++ Vaja zanke.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaja10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int x = 0;
+            long vsota=0;
+            double obresti = 0;
+            for (int i = 0; i < 100000; i++)
+                {
+                x = r.Next(1000000);
+                vsota = vsota  + x;
+                //za vsoto vsega
+                obresti = obresti + (x / 100 * 2.3);
+
+                }
+            Console.WriteLine((vsota / 100 * 2.3) + " je 2.3% seštevka vseh računov");
+            Console.WriteLine(obresti +" je račun usake posamezne obresti skupaj");
+            Console.ReadLine();
+        }
+            
+
+            
+        }
+    }
