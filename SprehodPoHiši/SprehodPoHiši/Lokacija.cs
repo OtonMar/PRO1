68b6da9fa6e42595c64a5e9b987abf8c1245de9d
“airaction” <otonmar201@gmail.com>
2023-03-03 10:18:36 +0100

    vaja

---
+++ SprehodPoHiši/SprehodPoHiši/Lokacija.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprehodPoHiši
+{
+    internal class Lokacija
+    {
+        public string ime;
+        public Lokacija[] izhodi;
+        public Lokacija(string i)
+        {
+            ime = i;
+        }
+        //trenutno si v dnevni sobi, od tu lahko greš v jedilnico, sprednje dvorišče
+        public string Opis()
+        {
+            string r = "";
+            r += "trenutno si v " + ime;
+            r += Environment.NewLine;
+            r += "Od tu lahko greš: ";
+            r += Environment.NewLine;
+            for(int k=0; k<izhodi.Length; k++)
+            {
+
+                r += k + " - " + izhodi[k].ime;
+                r += Environment.NewLine;
+            }
+            return r;
+        }
+    }
+}
---
+++ SprehodPoHiši/SprehodPoHiši/Zunanji.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprehodPoHiši
+{
+    internal class Zunanji:Lokacija
+    {
+        bool jeVroče;
+        public Zunanji(string i, bool v) : base(i)
+        {
+            jeVroče = v;
+        }
+        public new string Opis()
+        {
+            string r = base.Opis();
+            r += Environment.NewLine;
+            r += "Je vroče? " + jeVroče + Environment.NewLine;
+            return r;
+        }
+    }
+}
